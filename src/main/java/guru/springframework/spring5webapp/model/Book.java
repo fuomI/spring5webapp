b42e4edde14f0b277c0c2192f1d061656e0dd91c
Joni Jansson <joni.jansson@saunalahti.fi>
2023-01-26 10:08:13 +0200

    Author.java & Book.java: Fixed toString() methods

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -103,7 +103,6 @@ public class Book {
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
                 ", publisher='" + publisher + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 }
