be7e9a494408ca5c9cc91cfa82460fa6109cd3da
Joni Jansson <joni.jansson@saunalahti.fi>
2023-01-26 10:01:54 +0200

    BookController.java: Whitespace trimming

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,6 +1,5 @@
 package guru.springframework.spring5webapp.controllers;
 
-
 import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,7 +18,7 @@ public class BookController {
     public String getBooks(Model model) {
 
         model.addAttribute("books", bookRepository.findAll());
-        
+
         return "books";
     }
 }
